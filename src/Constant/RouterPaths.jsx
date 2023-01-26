3517989ee785ff3f75f718cfee906cc0e52261b1
Raj Gohil <raj.famous007@gmail.com>
2023-01-26 18:20:56 +0530

    Dynamic path routing for the github pages deployment

--- src/Constant/RouterPaths.jsx
+++ src/Constant/RouterPaths.jsx
@@ -1,5 +1,14 @@
-export const loginRoutePath = "/login";
+export const loginRoutePath =
+  process.env.NODE_ENV === "development"
+    ? "/login"
+    : "/static-login-sign-up-form-for-react-practice/login";
 
-export const signUpRoutePath = "/sign-up";
+export const signUpRoutePath =
+  process.env.NODE_ENV === "development"
+    ? "/sign-up"
+    : "/static-login-sign-up-form-for-react-practice/sign-up";
 
-export const homePath = "/";
+export const homePath =
+  process.env.NODE_ENV === "development"
+    ? "/"
+    : "/static-login-sign-up-form-for-react-practice/";
